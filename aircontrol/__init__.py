bc0cb1979db5d8e60eb3e39933eb5deec9f4a575
likianta <likianta@foxmail.com>
2024-08-08 10:52:03 +0800

    add third libs to path

--- aircontrol/__init__.py
+++ aircontrol/__init__.py
@@ -2,7 +2,11 @@ if 1:
     import lk_logger
     lk_logger.setup(quiet=True)
 
+if 2:
+    import sys
+    sys.path.append('third_libs')
+
 from .client import Client
+from .server import Server
 # from .serdes import dump
 # from .serdes import load
-from .server import Server
