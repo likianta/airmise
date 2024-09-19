9d6e0be10db3883ecc93a29909f2d95c8b9937f5
likianta <likianta@foxmail.com>
2024-09-19 12:11:26 +0800

    publish via depsland

--- test/client_test.py
+++ test/client_test.py
@@ -1,15 +1,17 @@
 from random import choice
 
+from argsense import cli
 from lk_logger import start_ipython
 
-from aircontrol import client
+import aircontrol as air
 
 
-def test() -> None:
-    client.open(False)
+@cli.cmd()
+def main(**kwargs) -> None:
+    air.connect(**kwargs)
     
     def _demo1() -> None:
-        ret = client.run(
+        ret = air.run(
             '''
             from random import randint
             class FakeDriver:
@@ -23,7 +25,7 @@ def test() -> None:
         print(hex(x), hex(ret[0]), hex(ret[1]))
     
     def _demo2() -> None:
-        ret = client.run(
+        ret = air.run(
             '''
             memo d
             return (reg, d.read(reg))
@@ -33,18 +35,17 @@ def test() -> None:
         print(hex(x), hex(ret[0]), hex(ret[1]))
     
     start_ipython({
-        'exe'  : client,
-        'run'  : client.run,
+        'air'  : air,
         'demo1': _demo1,
         'demo2': _demo2,
     })
 
 
 if __name__ == '__main__':
-    # 1. pox -m aircontrol run-client
+    # 1. pox -m aircontrol run-local-server
     # 2. pox test/client_test.py
     #   in ipython:
     #       demo1()
     #       demo2()
     #       ...
-    test()
+    main()
