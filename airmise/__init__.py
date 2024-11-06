50ec23a3c751a14d0eeb157679f6d696e2957148
likianta <likianta@foxmail.com>
2024-11-06 16:50:02 +0800

    refresh requirements.lock

--- airmise/__init__.py
+++ airmise/__init__.py
@@ -1,7 +1,9 @@
 if 1:
     import sys
-    sys.path.append('deps/core')
-    sys.path.append('deps/extra')
+    from os.path import exists
+    from lk_utils import xpath
+    if exists(x := xpath('../deps')):
+        sys.path.extend((f'{x}/core', f'{x}/extra'))
 
 if 2:
     import lk_logger
@@ -11,6 +13,7 @@ from . import const
 from .client import Client
 from .client import connect
 from .client import run
+from .client import run as call
 from .const import CLIENT_DEFAULT_PORT
 from .const import SERVER_DEFAULT_PORT
 from .const import WEBAPP_DEFAULT_PORT
@@ -19,6 +22,6 @@ from .serdes import dump
 from .serdes import load
 from .util import get_local_ip_address
 from .util import random_name
-from .webapp import UserLocalServer
-from .webapp import WebClient
-from .webapp import WebServer
+# from .webapp import UserLocalServer
+# from .webapp import WebClient
+# from .webapp import WebServer
