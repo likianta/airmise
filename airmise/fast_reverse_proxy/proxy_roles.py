bd41d364f87ed7d02078d55433eec0d9d3ca0a5d
Likianta <likianta@foxmail.com>
2026-09-02 09:58:10 +0800

    show readable info besides uid

--- airmise/fast_reverse_proxy/proxy_roles.py
+++ airmise/fast_reverse_proxy/proxy_roles.py
@@ -2,9 +2,11 @@
 https://chatgpt.com/share/6a965430-dab0-83ee-aac8-916c0bd374be
 """
 
+import os
 import typing as tp
 from types import FunctionType
 
+from lk_utils import now
 from lk_utils import uuid
 
 from .. import const
@@ -17,6 +19,7 @@ from ..responder import Responder
 from ..responder import T
 from ..socket_wrapper import Socket
 from ..socket_wrapper import SocketClosed
+from ..util import get_local_ip_address
 
 
 class Broker(Responder):
@@ -59,7 +62,8 @@ class Router(Server):
         self, host: str = const.DEFAULT_HOST, port: int = const.SERVER_PORT
     ) -> None:
         super().__init__(host, port)
-        self.routes: tp.Dict[str, Socket] = {}
+        self.routes: tp.Dict[str, tp.Tuple[Socket, dict]] = {}
+        #   {uid: (connection, user_info), ...}
 
     def _handle_connection(self, conn: Socket) -> None:
         data_bytes = conn.recvall()
@@ -69,13 +73,18 @@ class Router(Server):
             uid = data['uid']
             assert uid in self.routes, uid
             broker = self.connections[conn.port] = Broker(
-                source=conn, target=self.routes[uid]
+                source=conn, target=self.routes[uid][0]
             )
             broker.mainloop(blocking=False)
             conn.sendall(encode((const.NORMAL, 'ok')))
         elif event == 'register_proxy_callee':
-            uid = uuid()
-            self.routes[uid] = conn
+            data.update({'connection_port': conn.port, 'register_time': now()})
+            uid = uuid(
+                '{}@{}:{}'.format(
+                    data['user_name'], data['ip'], data['connection_port']
+                )
+            )
+            self.routes[uid] = (conn, data)
             conn.sendall(encode((const.NORMAL, uid)))
         else:
             # maybe regular client, see `../client.py:Client:_say_hi` and
@@ -87,7 +96,19 @@ class Router(Server):
 class Callee(Responder):
     def __init__(self, user_namespace: tp.Optional[T.Namespace] = None) -> None:
         super().__init__(None, user_namespace)  # type: ignore
-        self.uid = ''
+        self.user_name = os.getlogin()
+        self.user_ip = get_local_ip_address()
+        self.user_id = ''
+
+    @property
+    def uid(self) -> str:
+        assert self.user_id
+        return self.user_id
+
+    @property
+    def user_info(self) -> str:
+        assert self.socket
+        return '{}@{}:{}'.format(self.user_name, self.user_ip, self.socket.port)
 
     def connect(
         self,
@@ -106,9 +127,20 @@ class Callee(Responder):
         return self
 
     def _say_hi(self) -> None:
-        self._send(const.INTERNAL, 'register_proxy_callee')
-        self.uid = self._recv()
-        print(self.uid, ':nv2')
+        # self.user_info = '{}@{}:{}'.format(
+        #     os.getlogin(), get_local_ip_address(), self.socket.port
+        # )
+        self._send(
+            const.INTERNAL,
+            'register_proxy_callee',
+            {
+                'user_name': self.user_name,
+                'ip': self.user_ip,
+                'port': self.socket.port,
+            },
+        )
+        self.user_id = self._recv()
+        print(self.user_id, ':nv2')
 
 
 class Caller(Responder):
--- airmise/socket_wrapper.py
+++ airmise/socket_wrapper.py
@@ -1,15 +1,17 @@
 import socket
-import typing as t
+import typing as tp
+
 
 class SocketClosed(Exception):
     pass
+
 
 class Socket:
     host: str
     port: int
     verbose: bool
     _socket: socket.socket
-    
+
     def __init__(self, verbose: bool = False, **kwargs) -> None:
         self.verbose = verbose
         if '_socket' in kwargs:
@@ -21,29 +23,29 @@ class Socket:
             # del self.connect
         else:
             self._socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
+
     @property
     def url(self) -> str:
         return 'tcp://{}:{}'.format(self.host, self.port)
-    
+
     def accept(self) -> 'Socket':
         conn, addr = self._socket.accept()
         new_socket = Socket(_socket=conn, host=addr[0], port=addr[1])
         print(
             'new connection accepted',
             '{} <- {}'.format(self.url, new_socket.url),
-            ':v'
+            ':v',
         )
         return new_socket
-    
+
     def bind(self, host: str, port: int) -> None:
         self.host = host
         self.port = port
         self._socket.bind((host, port))
-    
+
     def close(self) -> None:
         self._socket.close()
-    
+
     def connect(
         self, server_host: str, server_port: int, timeout: int = 0
     ) -> None:
@@ -60,8 +62,9 @@ class Socket:
             print(
                 ':v8p',
                 'cannot connect to server via "{}"! '
-                'please check if server online.'
-                .format('tcp://{}:{}'.format(server_host, server_port))
+                'please check if server online.'.format(
+                    'tcp://{}:{}'.format(server_host, server_port)
+                ),
             )
             raise
         else:
@@ -70,17 +73,20 @@ class Socket:
             # see a workaround in `build/build_standalone/airclient_standalone/
             # src/client.py`.
             self.host, self.port = self._socket.getsockname()
-            print(':pv4', 'connected to server: {} <- {}'.format(
-                'tcp://{}:{}'.format(server_host, server_port), self.url
-            ))
-    
+            print(
+                ':pv4r',
+                'connected to server: {} [green dim]<- {}[/]'.format(
+                    'tcp://{}:{}'.format(server_host, server_port), self.url
+                ),
+            )
+
     def listen(self, backlog: int = 1) -> None:
         self._socket.listen(backlog)
         print(':pv2', 'server is listening at {}'.format(self.url))
-    
+
     def recvall(self) -> bytes:
         size_width = int(self._socket.recv(1))
-        '''
+        """
             digits  max_hex     max_size
             ------  ----------  --------
             0       .           CLOSED
@@ -93,14 +99,14 @@ class Socket:
             7       FFFFFFF     256MB
             8       FFFFFFFF    4GB
             9       FFFFFFFFF   64GB
-        '''
+        """
         if size_width == 0:
             print(':pv7', 'remote request closing this connection', self.url)
             self.sendall(b'ok')
             self._socket.close()
             print(':pv3', 'port released', self.port)
             raise SocketClosed
-        
+
         exact_size = int(self._socket.recv(size_width), 16)
         # notice: https://stackoverflow.com/a/17668009/9695911
         # trick: https://poe.com/s/2HbNCYsmKHIqZqoQ6Md3
@@ -116,19 +122,19 @@ class Socket:
                 'recv',
                 size_width,
                 '{:X} ({})'.format(exact_size, _pretty_size(exact_size)),
-                _shortify_message(data_bytes)
+                _shortify_message(data_bytes),
             )
         return bytes(data_bytes)
-    
+
     def send_close_event(self) -> None:
         self._socket.sendall(b'0')
         assert self.recvall() == b'ok'
-    
+
     def sendall(self, msg: bytes) -> None:
         for datum in self._encode_message(msg):
             self._socket.sendall(datum)
-    
-    def _encode_message(self, data_bytes: bytes) -> t.Iterator[bytes]:
+
+    def _encode_message(self, data_bytes: bytes) -> tp.Iterator[bytes]:
         exact_size = '{:X}'.format(len(data_bytes))
         size_width = len(exact_size)
         assert 0 < size_width <= 9
@@ -138,13 +144,14 @@ class Socket:
                 'send',
                 size_width,
                 '{} ({})'.format(exact_size, _pretty_size(int(exact_size, 16))),
-                _shortify_message(data_bytes)
+                _shortify_message(data_bytes),
             )
         yield str(size_width).encode()
         yield exact_size.encode()
         yield data_bytes
 
-def _pretty_size(size: t.Union[int, float]) -> str:
+
+def _pretty_size(size: tp.Union[int, float]) -> str:
     for unit in ('B', 'KB', 'MB', 'GB'):
         if size < 1024:
             return f'{size:.2f}{unit}'
@@ -152,8 +159,9 @@ def _pretty_size(size: t.Union[int, float]) -> str:
     else:
         return f'{size:.2f}TB'
 
+
 def _shortify_message(
-    msg_in_bytes: t.Union[bytes, bytearray], chunk_size: int = 10
+    msg_in_bytes: tp.Union[bytes, bytearray], chunk_size: int = 10
 ) -> str:
     if len(msg_in_bytes) < chunk_size * 2:
         return msg_in_bytes.decode()
